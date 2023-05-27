d4b58c42448fe88e076eeca601a116d5480cc09a
fauzanpurwaw <fauzanpurwaw@gmail.com>
2023-05-27 15:01:25 +0700

    add filter modal

--- components/filter-modal.vue
+++ components/filter-modal.vue
@@ -6,32 +6,74 @@
     </button>
     <Teleport to="body">
         <Transition>
-        <div class="flex justify-center items-center w-full h-[100vh] z-50 bg-slate-700 bg-opacity-50 fixed"
-            v-show="filterOpen">
-                <div class="flex flex-col items-center bg-white h-[90vh] w-[760px] max-w-[800px] rounded-xl shadow-xl px-5">
-                    <div class="flex items-center  bg-red-100 w-full h-[10%] rounded-t-xl relative">
-                        <button @click="filterOpen = false" class="opacity-75 hover:opacity-100"><span><img src="/svg/close.svg" alt="" class="h-5"></span></button>
-                        <div class="text-lg font-semibold w-full text-center mr-5">Filter</div>
-                    </div>
-                    <div class="border bg-red-200 h-[80%] w-full overflow-y-auto py-4">
-                        <div>
-                            <h2 class="text-2xl font-semibold text-slate-800">Price range</h2>
-                            <p class="text-lg text-slate-600">The average nightly price is Rp1,145,983, not including fees or taxes.</p>
-                            <div class="flex justify-center items-center w-full p-5 mt-5">
-                                <input type="number" name="" id="" class="flex-1 w-full h-16 rounded-lg text-center">
-                                <p class="mx-5">s/d</p>
-                                <input type="number" name="" id="" class="flex-1 w-full h-16 rounded-lg text-center">
+            <div class="flex justify-center items-center w-full h-[100vh] z-20 fixed" v-show="filterOpen">
+                <div class="relative h-full w-full flex items-center justify-center">
+                    <div class="absolute h-screen w-screen bg bg-slate-700 bg-opacity-25 z-[1]" @click="filterOpen = false"></div>
+                    <div class="flex flex-col items-center bg-white h-[90vh] w-[760px] max-w-[800px] rounded-xl shadow-xl px-5 z-[2]">
+                        <div class="flex items-center bg-red-100 w-full h-[10%] rounded-t-xl relative">
+                            <button @click="filterOpen = false, pickedBed = 'bebas', pickedToilet = 'bebas'"
+                                class="opacity-75 hover:opacity-100"><span><img src="/svg/close.svg" alt=""
+                                        class="h-5"></span></button>
+                            <div class="text-lg font-semibold w-full text-center mr-5">Filter</div>
+                        </div>
+                        <div class="border bg-red-200 h-[80%] w-full overflow-y-auto py-4">
+                            <div>
+                                <h2 class="text-2xl font-semibold text-slate-800">Price range</h2>
+                                <p class="text-lg text-slate-600 font-light">The average nightly price is Rp1,145,983, not
+                                    including fees or taxes.</p>
+                                <div class="flex justify-center items-center w-full p-5 mt-5">
+                                    <input type="number" name="" id="" class="flex-1 w-full h-16 rounded-lg text-center">
+                                    <p class="mx-5">s/d</p>
+                                    <input type="number" name="" id="" class="flex-1 w-full h-16 rounded-lg text-center">
+                                </div>
+                            </div>
+                            <div class="mt-10">
+                                <h2 class="text-2xl font-semibold text-slate-800">Bed & Bedroom</h2>
+                                <div>
+                                    <p class="text-lg text-slate-600 font-light mt-5">Beds</p>
+                                    <form action="" class="flex items-center gap-2 px-2 mt-3">
+                                        <div v-for="p in bedntoiletList">
+                                            <label v-if="pickedBed == p" :for="p + 'bed'"
+                                                class="flex justify-center border rounded-full px-3 py-2 text-center bg-black text-white">
+                                                <input type="radio" :value="p" :id="p + 'bed'" name="bed" class="invisible"
+                                                    v-model="pickedBed">
+                                                <p class="mr-4 font-light">{{ p }}</p>
+                                            </label>
+                                            <label v-else :for="p + 'bed'"
+                                                class="flex justify-center border rounded-full px-3 py-2 text-center">
+                                                <input type="radio" :value="p" :id="p + 'bed'" name="bed" class="invisible"
+                                                    v-model="pickedBed">
+                                                <p class="mr-4 font-light">{{ p }}</p>
+                                            </label>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div>
+                                    <p class="text-lg text-slate-600 font-light mt-5">Rest Room</p>
+                                    <form action="" class="flex items-center gap-2 px-2 mt-3">
+                                        <div v-for="p in bedntoiletList">
+                                            <label v-if="pickedToilet == p" :for="p + 'toilet'"
+                                                class="flex justify-center border rounded-full px-3 py-2 text-center bg-black text-white">
+                                                <input type="radio" :value="p" :id="p + 'toilet'" name="toilet" class="invisible"
+                                                    v-model="pickedToilet">
+                                                <p class="mr-4 font-light">{{ p }}</p>
+                                            </label>
+                                            <label v-else :for="p + 'toilet'"
+                                                class="flex justify-center border rounded-full px-3 py-2 text-center">
+                                                <input type="radio" :value="p" :id="p + 'toilet'" name="toilet" class="invisible"
+                                                    v-model="pickedToilet">
+                                                <p class="mr-4 font-light">{{ p }}</p>
+                                            </label>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="flex items-center w-full p-5 mt-5">
+                                </div>
                             </div>
                         </div>
-                        <div class="mt-10">
-                            <h2 class="text-2xl font-semibold text-slate-800">Bed & Bedroom</h2>
-                            <p class="text-lg text-slate-600">Beds</p>
-                            <div class="flex items-center w-full p-5 mt-5">
-                            </div>
+                        <div class="bg-red-100 w-full h-[10%] rounded-b-xl">
+    
                         </div>
-                    </div>
-                    <div class="bg-red-100 w-full h-[10%] rounded-b-xl">
-
                     </div>
                 </div>
             </div>
@@ -43,6 +85,13 @@
 export default {
     props: {
         filterOpen: Boolean,
-    }
+    },
+    data() {
+        return {
+            bedntoiletList: ["bebas", "1", "2", "3", "4", "5", "6", "7", "8+"],
+            pickedBed: "bebas",
+            pickedToilet: "bebas",
+        }
+    },
 }
 </script>
--- components/mid-header.vue
+++ components/mid-header.vue
@@ -3,7 +3,8 @@
         <div class="w-full flex justify-between px-2 mx-auto font-semibold text-lg">
             <div class="flex flex-row justify-center items-center" >  
                 <img src="/svg/airbnb.svg" alt="BnBlogo" srcset="" class="h-8 lg:block hidden">
-                <img src="/svg/airbnb-1.svg" alt="BnBlogo" srcset="" class="h-10 lg:hidden">
+                <img src="/svg/airbnb-1.svg" alt="BnBlogo" srcset="" class="h-10 lg:hidden sm:block hidden">
+                <!-- searchbar desktop -->
                 <div class="lg:flex flex-row justify-center items-center 
                 gap-2 h-full ml-10 px-5 text-sm border border-gray-300 rounded-full
                 hover:shadow hover:cursor-pointer w-fit hidden font-normal">
@@ -12,10 +13,15 @@
                     <a >Tambah tamu</a>
                     <span class="bg-red-bnb px-2 py-1 rounded-full"><img src="/img/magnifying-glass.png" alt=""  class="h-4 inline"></span>
                 </div>
+                <!-- searchbar mobile -->
+                <div class="sm:hidden flex justify-between items-center border border-red-400 rounded-full w-60 p-1 pl-3 text-small font-normal">
+                    Cari disini...
+                    <span class="flex items-center justify-center bg-red-bnb p-2 rounded-full"><img src="/img/magnifying-glass.png" alt=""  class="h-4 inline"></span>
+                </div>
             </div>
             <div class="flex justify-between items-center gap-4 text-sm">
-                <a href="" class="hidden sm:block">Jadikan rumah Anda Airbnb</a>
-                <a href=""><span><img src="/svg/globe.svg" alt="" class="h-5 inline"></span></a>
+                <a href="" class="hidden">Jadikan rumah Anda Airbnb</a>
+                <a href="" class="ml-2"><span><img src="/svg/globe.svg" alt="" class="h-5 inline"></span></a>
                 <DropdownMenu />
             </div>
         </div>
